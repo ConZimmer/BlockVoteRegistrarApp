bfe80cca159e0eaeedc197fc79292dd86266b156
Michael Heiber <mt.heiber@gmail.com>
2017-02-13 16:31:31 -0700

    Crash on signing token

--- app/src/main/java/com/blockvote/registrarapplication/qrCode/GenerateQRActivity.java
+++ app/src/main/java/com/blockvote/registrarapplication/qrCode/GenerateQRActivity.java
@@ -2,6 +2,7 @@ package com.blockvote.registrarapplication.qrCode;
 
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -14,8 +15,15 @@ import com.google.zxing.BarcodeFormat;
 import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
+import android.util.Base64;
+import com.google.gson.Gson;
+import android.content.SharedPreferences;
 
 import com.blockvote.registrarapplication.R;
+
+import com.blockvote.registrarapplication.crypto.*;
+
+import java.math.BigInteger;
 
 public class GenerateQRActivity extends AppCompatActivity {
 
@@ -28,6 +36,11 @@ public class GenerateQRActivity extends AppCompatActivity {
     private Thread thread ;
     private final static int QRcodeWidth = 1000 ;
     private Bitmap bitmap ;
+    private String blindedToken;
+    private String signedToken;
+    private TokenRequest tokenRequest;
+    private byte[] signature;
+    private SharedPreferences dataStore;
 
     GenerateQRActivity generateQRActivity;
 
@@ -43,6 +56,41 @@ public class GenerateQRActivity extends AppCompatActivity {
         textView = (TextView)findViewById(R.id.textViewResults);
 
         textView.setText(getIntent().getStringExtra("Value"));
+        blindedToken = getIntent().getStringExtra("Value");
+
+        tokenRequest = new TokenRequest(Base64.decode(blindedToken, Base64.DEFAULT));
+
+        dataStore = getPreferences(MODE_PRIVATE);
+
+        Gson gson = new Gson();
+        //TODO: Move to login fragment
+        SharedPreferences.Editor prefsEditor = dataStore.edit();
+
+        String publicKeyModulusS="AKBDi814o+/Ujo8bF1qjMnyotruZHLv5FWl/xYYFpKLgU4jmeXxhJKY36kmJFK+Kxt6anqmmVBKVZityfp+2lUzojYEJJ9Jzv4qQQQ4BcHijlrrBSvvWZ6KOVB30n2Lgxj99g6B1Eopyq4h+6TC3Sr/DBIkZ0tAH5a3+RG3Q8OEcYGpCQu7v4MIOgF+bFikeu6gk0Mob71TlPGauAIFpc4q3UVBjhbEIyc6vv76Z+RtNd3FZZzsLphzrJB4s6b6TwKpUsIWJ7dXBpkCSVv/sDtB4PeOrzHTH5UHGYkTLbF4o1ie23mbjhIWcSJryJrS+3VMaNuB+waImz/nlJEh/qy0=";
+        String publicKeyExponentS="AQAB";
+        String privateKeyModulusS="AKBDi814o+/Ujo8bF1qjMnyotruZHLv5FWl/xYYFpKLgU4jmeXxhJKY36kmJFK+Kxt6anqmmVBKVZityfp+2lUzojYEJJ9Jzv4qQQQ4BcHijlrrBSvvWZ6KOVB30n2Lgxj99g6B1Eopyq4h+6TC3Sr/DBIkZ0tAH5a3+RG3Q8OEcYGpCQu7v4MIOgF+bFikeu6gk0Mob71TlPGauAIFpc4q3UVBjhbEIyc6vv76Z+RtNd3FZZzsLphzrJB4s6b6TwKpUsIWJ7dXBpkCSVv/sDtB4PeOrzHTH5UHGYkTLbF4o1ie23mbjhIWcSJryJrS+3VMaNuB+waImz/nlJEh/qy0=";
+        String privateKeyExponentS="GsCDuPoDJZDd9invyU+2KQRxslmB6CfRSPHM600MWSrojtDoJRjDKSLqzzkcdJwOC9EUHJ4Y6Rw6uJRtaiQsgnDMVCaO2Oy847imP1wCpgSqr8R9y5GT7ZjkFjcEFxmNxkHho7p/LJCtLQUAUIM8LUv0uR0QKW00DAoGaq1m1DCog+PzxRhsJvx6zx6yw7+xwyKd1udXErACRVyvplJ7nFvcfBSC0fp4V3kf1OxUbLAxPVYxiD2LXq6ND3GQY0kT+WueHd5AKSBXv3a21KRXLrhUn22EYsm3oY6pAD/6WiwXdmMpdqgA8NeLOD0O8qbpZogM8VRwuKV4fZZ1fpLeCQ==";
+
+        BigInteger publicKeyModulus = new BigInteger(Base64.decode(publicKeyModulusS, Base64.DEFAULT));
+        BigInteger publicKeyExponent = new BigInteger(Base64.decode(publicKeyExponentS, Base64.DEFAULT));
+        BigInteger privateKeyModulus = new BigInteger(Base64.decode(privateKeyModulusS, Base64.DEFAULT));
+        BigInteger privateKeyExponent = new BigInteger(Base64.decode(privateKeyExponentS, Base64.DEFAULT));
+
+
+        Registrar registrarInput = new Registrar(publicKeyModulus, publicKeyExponent, privateKeyModulus, privateKeyExponent);
+        String json = gson.toJson(registrarInput);
+        prefsEditor.putString("registrar", json);
+        prefsEditor.commit();
+
+        json = dataStore.getString("registrar", "");
+        if(json==null){
+            Log.e("ERROR", "ERROR json is null");
+        }
+        Registrar registrar = gson.fromJson(json, Registrar.class);
+
+        byte[] temp = registrar.sign(tokenRequest);
+
+        signedToken = Base64.encodeToString(temp, Base64.DEFAULT);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -53,7 +101,7 @@ public class GenerateQRActivity extends AppCompatActivity {
 
                 try {
                     //TODO: Do this processing using AsyncTask
-                    bitmap = TextToImageEncode(EditTextValue);
+                    bitmap = TextToImageEncode(signedToken);
 
                     imageView.setImageBitmap(bitmap);
 
