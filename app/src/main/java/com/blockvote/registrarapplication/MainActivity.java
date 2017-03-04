d25bab74b5671199e3d5dbb3a919520d8bf8a957
Michael Heiber <mt.heiber@gmail.com>
2017-03-03 19:31:57 -0700

    Added tabs on the main screen

--- app/src/main/java/com/blockvote/registrarapplication/MainActivity.java
+++ app/src/main/java/com/blockvote/registrarapplication/MainActivity.java
@@ -6,9 +6,11 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
+import android.widget.TabHost;
 
 import com.blockvote.registrarapplication.qrCode.ReadQRActivity;
 
@@ -25,11 +27,41 @@ public class MainActivity extends AppCompatActivity {
         //ActionBar myActionBar = (ActionBar) getSupportActionBar();
         //myActionBar.setTitle("Testing");
 
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        setSupportActionBar(myToolbar);
+        ActionBar myActionBar = (ActionBar) getSupportActionBar();
+        myActionBar.setTitle("Welcome 'registar Name goes here' ");
+
+        //Progress Bar
+        /*
         View theView = (View) findViewById(R.id.progressBarMain);
         ProgressBar pb = (ProgressBar) theView.findViewById(R.id.progressBar);
 
         pb.setProgress(80);
         pb.setScaleY(2f);
+        */
+
+
+        TabHost host = (TabHost)findViewById(R.id.main_tab_menu);
+        host.setup();
+
+        //Tab 1
+        TabHost.TabSpec spec = host.newTabSpec("Tab One");
+        spec.setContent(R.id.tab1_register_voter);
+        spec.setIndicator("Add Voter");
+        host.addTab(spec);
+
+        //Tab 2
+        spec = host.newTabSpec("Tab Two");
+        spec.setContent(R.id.tab2_incomplete_registrations);
+        spec.setIndicator("Incomplete");
+        host.addTab(spec);
+
+        //Tab 3
+        spec = host.newTabSpec("Tab Three");
+        spec.setContent(R.id.tab3_completed_registrations);
+        spec.setIndicator("Completed");
+        host.addTab(spec);
 
 
         dataStore = getPreferences(MODE_PRIVATE);
