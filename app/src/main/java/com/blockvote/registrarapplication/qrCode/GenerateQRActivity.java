0919a2f4c45eb1da2edc5afc6a506d24554d97fe
Michael Heiber <mt.heiber@gmail.com>
2017-03-05 16:02:46 -0700

    Async QR generation with animation

--- app/src/main/java/com/blockvote/registrarapplication/qrCode/GenerateQRActivity.java
+++ app/src/main/java/com/blockvote/registrarapplication/qrCode/GenerateQRActivity.java
@@ -1,6 +1,8 @@
 package com.blockvote.registrarapplication.qrCode;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.support.v7.app.AppCompatActivity;
@@ -46,6 +48,10 @@ public class GenerateQRActivity extends AppCompatActivity {
         generateQRActivity = this;
 
         imageView = (ImageView)findViewById(R.id.image_QRCode);
+        imageView.setVisibility(View.GONE);
+
+        View rootView = this.findViewById(android.R.id.content);
+
 
         blindedTokenString = getIntent().getStringExtra("ScannedQRCodeBlindedTokenString");
 
@@ -71,9 +77,9 @@ public class GenerateQRActivity extends AppCompatActivity {
         {
             Log.e("ERROR", "Error reading QR code or signing token");
         }
-
-
-
+        
+        new QRGenerator(rootView).execute(signedToken);
+/*
         try {
             //TODO: Do this processing using AsyncTask
             bitmap = TextToImageEncode(signedToken);
@@ -83,47 +89,80 @@ public class GenerateQRActivity extends AppCompatActivity {
         } catch (WriterException e) {
             e.printStackTrace();
         }
-
+*/
     }
 
-    Bitmap TextToImageEncode(String Value) throws WriterException {
-        BitMatrix bitMatrix;
-        try {
-            bitMatrix = new MultiFormatWriter().encode(
-                    Value,
-                    BarcodeFormat.DATA_MATRIX.QR_CODE,
-                    QRcodeWidth, QRcodeWidth, null
-            );
 
-        } catch (IllegalArgumentException Illegalargumentexception) {
+    public class QRGenerator extends AsyncTask<String, Void, Bitmap> {
+        private final String LOG_TAG= "QRGenerator";
+        private View rootView;
 
-            return null;
-        }
-        catch (Exception ex) {
-            Log.e("ERROR", "Catching exception when attempting to create QR code");
-            return null;
+        public QRGenerator(View rootView){
+            this.rootView=rootView;
+            Log.e(LOG_TAG, "QR generation started... " );
         }
 
-        int bitMatrixWidth = bitMatrix.getWidth();
-
-        int bitMatrixHeight = bitMatrix.getHeight();
-
-        Log.v(LOG_TAG, "QR Width: " + bitMatrixWidth + ", Height: " + bitMatrixHeight);
-        int[] pixels = new int[bitMatrixWidth * bitMatrixHeight];
-
-        for (int y = 0; y < bitMatrixHeight; y++) {
-            int offset = y * bitMatrixWidth;
-
-            for (int x = 0; x < bitMatrixWidth; x++) {
-//                getResources().getColor(R.color.QR)
-                pixels[offset + x] = bitMatrix.get(x, y) ?
-                        getResources().getColor(R.color.QRDarkColor):getResources().getColor(R.color.QRWhite);
-
+        @Override
+        public Bitmap doInBackground (String... params) {
+            try{
+                Bitmap bitmap= TextToImageEncode(params[0]);
+                return bitmap;
+            }catch(WriterException writerException){
+                Log.e(LOG_TAG, "QR generation failed... " + "\n" + writerException.getMessage() );
+                return null;
             }
-        }
-        Bitmap bitmap = Bitmap.createBitmap(bitMatrixWidth, bitMatrixHeight, Bitmap.Config.ARGB_4444);
 
-        bitmap.setPixels(pixels, 0, bitMatrixWidth, 0, 0, bitMatrixWidth, bitMatrixHeight);
-        return bitmap;
+        }
+
+        @Override
+        public void onPostExecute(Bitmap bitmap){
+            Log.v(LOG_TAG, "Displaying the QR now");
+            ImageView imageView = (ImageView) rootView.findViewById(R.id.image_QRCode);
+            imageView.setImageBitmap(bitmap);
+
+            rootView.findViewById(R.id.image_QRCode).setVisibility(View.VISIBLE);
+            rootView.findViewById(R.id.QR_animation_view).setVisibility(View.GONE);
+        }
+
+        private Bitmap TextToImageEncode(String Value) throws WriterException {
+            BitMatrix bitMatrix;
+            try {
+                bitMatrix = new MultiFormatWriter().encode(
+                        Value,
+                        BarcodeFormat.DATA_MATRIX.QR_CODE,
+                        QRcodeWidth, QRcodeWidth, null
+                );
+
+            } catch (IllegalArgumentException Illegalargumentexception) {
+
+                return null;
+            }
+            catch (Exception ex) {
+                Log.e("ERROR", "Catching exception when attempting to create QR code");
+                return null;
+            }
+
+            int bitMatrixWidth = bitMatrix.getWidth();
+
+            int bitMatrixHeight = bitMatrix.getHeight();
+
+            Log.v(LOG_TAG, "QR Width: " + bitMatrixWidth + ", Height: " + bitMatrixHeight);
+            int[] pixels = new int[bitMatrixWidth * bitMatrixHeight];
+
+            for (int y = 0; y < bitMatrixHeight; y++) {
+                int offset = y * bitMatrixWidth;
+
+                for (int x = 0; x < bitMatrixWidth; x++) {
+//                getResources().getColor(R.color.QR)
+                    pixels[offset + x] = bitMatrix.get(x, y) ?
+                            getResources().getColor(R.color.QRDarkColor):getResources().getColor(R.color.QRWhite);
+
+                }
+            }
+            Bitmap bitmap = Bitmap.createBitmap(bitMatrixWidth, bitMatrixHeight, Bitmap.Config.ARGB_4444);
+
+            bitmap.setPixels(pixels, 0, bitMatrixWidth, 0, 0, bitMatrixWidth, bitMatrixHeight);
+            return bitmap;
+        }
     }
 }
